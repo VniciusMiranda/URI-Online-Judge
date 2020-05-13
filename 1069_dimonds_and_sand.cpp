1050e44b799ff96b50fd6fbc762bac89be8e4a2e
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-05-13 02:34:19 -0300

    finishing

--- 1069_dimonds_and_sand.cpp
+++ 1069_dimonds_and_sand.cpp
@@ -8,14 +8,13 @@ int main(){
 	int test;
 	std::cin >> test;
 	while(test--){
-
 		std::cin >> line;
 		int smaller_than = 0, dimonds = 0;
 		int i = 0;
 		while(line[i]){
 			if(line[i] == '<')	
 				smaller_than++;
-			if(line[i] == '>'){
+			if(line[i] == '>' && smaller_than > 0){
 				dimonds++;
 				smaller_than--;
 			}
