6b866f83e72866408601a1e2de2f74042cdbd322
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-07-07 23:43:26 -0300

    adding solution for the problem 1068

--- 1068_Parenthesis_Balance_I.cpp
+++ 1068_Parenthesis_Balance_I.cpp
@@ -4,7 +4,31 @@
 using namespace std;
 
 int main(){
-    
+
+    string s;
+    int p, ss;
+
+
+    while(getline(cin, s)){
+        ss = s.size();
+        p = 0;
+
+        for(int i = 0; i < ss; i++){
+
+            if(s[i] == '(')
+                p++;
+
+            if(s[i] == ')')
+                !(p > 0) ? p++ : p--;
+            
+        }
+
+    if(!(p > 0)) 
+        cout << "correct"<< endl;
+    else 
+        cout << "incorrect" << endl;
+
+    }
 
     return 0;
 }
